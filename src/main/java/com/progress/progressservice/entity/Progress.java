30cc2251b7f299328f5e5305760d93954d987cf6
Vipin Sukumaran <Vipin.Sukumaran@neudesic.com>
2024-12-31 11:50:15 +0530

    Initial commit

---
+++ src/main/java/com/progress/progressservice/entity/Progress.java
@@ -0,0 +1,27 @@
+package com.progress.progressservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Progress {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int progressId;
+
+    private Integer progressCourseId;
+
+    private Integer progressRemDuration;
+
+    @Column(length = 50)
+    private String progressCourseStatus;
+
+    private Integer progressNoModCompleted;
+
+    private Integer progressNoChaCompleted;
+}
