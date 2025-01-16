dfbdde99ad1bcf6fb0f73f325803df479fa5bc83
vipinms-crypto <vipin.sukumaran@neudesic.com>
2025-01-17 00:33:07 +0530

    Merge branch 'develop' of https://github.com/vipinms-crypto/progress-service.git into develop

---
+++ src/main/java/com/progress/progressservice/exception/ServiceLayerException.java
@@ -0,0 +1,14 @@
+package com.progress.progressservice.exception;
+
+public class ServiceLayerException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2846899683496295710L;
+	
+	public ServiceLayerException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/progress/progressservice/service/ProgressService.java
+++ src/main/java/com/progress/progressservice/service/ProgressService.java
@@ -3,23 +3,24 @@ package com.progress.progressservice.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.progress.progressservice.dto.requestDto.ProgressRequestDto;
-import com.progress.progressservice.dto.responseDto.ProgressResponseDto;
+import com.progress.progressservice.dto.requestdto.ProgressRequestDto;
+import com.progress.progressservice.dto.responsedto.ProgressResponseDto;
+import com.progress.progressservice.exception.ServiceLayerException;
 
 public interface ProgressService {
 
 	 // Create a new progress
-    ProgressResponseDto createProgress(ProgressRequestDto progressRequestDto) throws Exception;
+    ProgressResponseDto createProgress(ProgressRequestDto progressRequestDto) throws ServiceLayerException;
 
     // Get all progresss
-    List<ProgressResponseDto> getAllProgresss() throws Exception;
+    List<ProgressResponseDto> getAllProgresss() throws ServiceLayerException;
 
     // Get a progress by ID
-    Optional<ProgressResponseDto> getProgressById(Integer id) throws Exception;
+    Optional<ProgressResponseDto> getProgressById(Integer id) throws ServiceLayerException;
 
     // Update a progress
-    ProgressResponseDto updateProgress(Integer id, ProgressRequestDto progressRequestDto) throws Exception;
+    ProgressResponseDto updateProgress(Integer id, ProgressRequestDto progressRequestDto) throws ServiceLayerException;
 
     // Delete a progress
-    boolean deleteProgress(Integer id) throws Exception;
+    boolean deleteProgress(Integer id) throws ServiceLayerException;
 }
